6fbb4d40e9e609d3c7eb54dd1d727a3c070027b3
luka.napotnik <luka.napotnik@7839ed2c-3121-0410-985d-3ba8a6ab8d54>
2007-04-27 14:25:19 +0000

    Added lowlevel network i/o functions

---
+++ trunk/server/src/netio.h
@@ -0,0 +1,71 @@
+#ifndef __NETIO_H_
+#define __NETIO_H_
+
+
+#include <gnutls/gnutls.h>
+#include <glib-2.0/glib.h>
+
+/* Got that from Linux kernel ;) */
+#define PACKET_MAGIC 0xDEADBEEF
+#define WON_SERVER_PORT 5252
+#define DH_BITS 1024
+
+/* Possible credentials. */
+#define CRED_ANON 0x1
+#define CRED_CERT 0x2
+#define CRED_PASS 0x3
+#define CRED_OPGP 0x4
+
+static gnutls_dh_params_t dh_params;
+gnutls_anon_server_credentials_t anoncred;
+
+struct security_s {
+	gnutls_session_t session;
+	/* Possible credentials. */
+	gnutls_anon_client_credentials_t anoncred;
+    int listener;
+};
+
+/* Packet header information. */
+typedef struct {
+	long magic;
+	int reserved;
+	unsigned int level;
+	long long len;
+} packet_t;
+
+/*
+ * Initializes GnuTLS session and sets credentials.
+ */
+struct security_s *network_server_init(int security_policy);
+
+/*
+ * Get the server into a loop. 
+ */
+int
+network_server_loop(struct security_s *security);
+
+/*
+ * Creates a new TLS session.
+ */
+gnutls_session_t 
+network_session();
+
+/*
+ * Sends package.
+ */
+long long
+network_send(unsigned int level,
+             void *data,
+             long long len,
+             gnutls_session_t session);
+
+/*
+ * Recieves package.
+ */
+long long
+network_recv(unsigned int *level,
+             void **data,
+             gnutls_session_t session);
+
+#endif
