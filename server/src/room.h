c6966f95cedd12779dd9e39eca799aed71a5e344
luka.napotnik <luka.napotnik@7839ed2c-3121-0410-985d-3ba8a6ab8d54>
2007-04-29 19:13:57 +0000

    User and room functions

---
+++ server/src/room.h
@@ -0,0 +1,29 @@
+#ifndef __ROOM_H_
+#define __ROOM_H_
+
+#include <glib-2.0/glib.h>
+#include "netio.h"
+
+#define MAX_ROOMS 100
+struct room_s {
+    char name[32];
+    GList *users;
+};
+
+struct room_s rooms[MAX_ROOMS];
+/*
+ * Creates a new room with sd (socket descriptor) of the user as the admin.
+ */
+int
+room_create(char *name, int creator_sd);
+
+/* Destroys a room. */
+int
+room_destroy(char *name);
+
+/* Distributes data to all users in the room. */
+int
+room_distribute(int sd, void *data)
+
+#endif
+
